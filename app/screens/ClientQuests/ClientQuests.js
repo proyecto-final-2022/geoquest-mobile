3d98008dd97642f2be60bf4f5a065b2dd3ec5057
PabloOve <31904880+PabloOve@users.noreply.github.com>
2022-08-28 19:28:07 -0300

    ClientQuests wip

--- app/screens/ClientQuests/ClientQuests.js
+++ app/screens/ClientQuests/ClientQuests.js
@@ -11,8 +11,22 @@ const ClientQuests = ({route, navigation}) => {
 
     const {ID, name, image} = route.params
 
+    const [data, setData] = useState([])
+
+    const url = Config.appUrl + "clients/" + ID + "/quests"
+
+    useEffect(() => {
+        fetch(url)
+        .then((response) => response.json())
+        .then((json) => setData(json))
+        .catch((error) => console.error(error))
+        .finally(()=>setLoading(false))
+    }, [])
+
     return (
+        <ScrollView style={styles.view}>
         <Text style = {styles.title}>{name}</Text> 
+        </ScrollView> 
     )
 }
 
@@ -21,6 +35,10 @@ const styles = StyleSheet.create({
         fontSize: 20,
         fontWeight: 'bold',
         color: '#a52a2a',
+    },
+    view: {
+        flex: 1,
+        backgroundColor: '#FFF9CA',
     }
 });
 
