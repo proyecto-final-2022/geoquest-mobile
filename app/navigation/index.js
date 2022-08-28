246c36b2a6c012e0f1caf288d9069ed2e19526fc
isarasibar <sarasibar.ignacio@gmail.com>
2022-08-28 19:09:35 -0300

    Menu v1.0

--- app/navigation/index.js
+++ app/navigation/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import {NavigationContainer} from '@react-navigation/native'
+import {
+    NavigationContainer
+} from '@react-navigation/native'
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
 import SignInScreen from '../screens/SignInScreen'
 import SignUpScreen from '../screens/SignUpScreen'
@@ -7,18 +9,24 @@ import QuestNavigator from '../screens/QuestNavigator'
 import HomeScreen from '../screens/HomeScreen'
 import Scene from '../scenes/HelloUser'
 
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { DrawerContent } from '../screens/DrawerContent/DrawerContent';
+
+const Drawer = createDrawerNavigator();
+
 const Stack = createNativeStackNavigator()
 
 const Navigation = () => {
     return(
         <NavigationContainer>
-            <Stack.Navigator screenOptions={{headerShown: false}}>
-                <Stack.Screen name="Sign In" component={SignInScreen} />
-                <Stack.Screen name="Sign Up" component={SignUpScreen} />
-                <Stack.Screen name="Home" component={HomeScreen} />
-                <Stack.Screen name="Quest Navigator" component={QuestNavigator} />
-                <Stack.Screen name="Scene" component={Scene} />
-            </Stack.Navigator>
+                
+            <Drawer.Navigator screenOptions={{headerShown: false}} drawerContent={props => <DrawerContent {...props} />}>
+                <Stack.Screen name="Sign In" component={SignInScreen} options={{ swipeEnabled: false }}/>
+                <Stack.Screen name="Sign Up" component={SignUpScreen} options={{ swipeEnabled: false }}/>
+                <Drawer.Screen name="Home" component={HomeScreen} options={{ headerShown: true }}/>
+                <Drawer.Screen name="Quest Navigator" component={QuestNavigator} options={{ headerShown: true }}/>
+                <Drawer.Screen name="Scene" component={Scene} options={{ headerShown: true }}/>
+            </Drawer.Navigator>
         </NavigationContainer>
     )
 }
---
+++ app/screens/DrawerContent/DrawerContent.js
@@ -0,0 +1,183 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import {
+    DrawerContentScrollView,
+    DrawerItem
+} from '@react-navigation/drawer';
+import {
+    useTheme,
+    Avatar,
+    Title,
+    Caption,
+    Paragraph,
+    Drawer,
+    Text,
+    TouchableRipple,
+    Switch
+} from 'react-native-paper';
+import {CloseSession} from '../../storage/storage';
+import {useNavigation} from '@react-navigation/native'
+
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+
+export function DrawerContent(props) {
+
+    const navigation = useNavigation()
+
+    return(
+        <View style={{flex:1,backgroundColor: '#FFF9CA'}}>
+            <DrawerContentScrollView {...props}>
+                <View style={styles.drawerContent}>
+                    <View style={styles.userInfoSection}>
+                        <View style={{flexDirection:'row',marginTop: 15}}>
+                            <Avatar.Image 
+                                source={{
+                                    uri: 'https://img.olympicchannel.com/images/image/private/f_auto/t_1-1_300/primary/wfrhxc0kh2vvq77sonki'
+                                }}
+                                size={50}
+                            />
+                            <View style={{marginLeft:15, flexDirection:'column'}}>
+                                <Title style={styles.title}>Lionel Messi</Title>
+                                <Caption style={styles.caption}>@messirve10</Caption>
+                            </View>
+                        </View>
+
+                        <View style={styles.row}>
+                            <View style={styles.section}>
+                                <Paragraph style={[styles.paragraph, styles.caption]}>100M</Paragraph>
+                                <Caption style={styles.caption}>Amigos</Caption>
+                            </View>
+                            <View style={styles.section}>
+                                <Paragraph style={[styles.paragraph, styles.caption]}>200</Paragraph>
+                                <Caption style={styles.caption}>Notificaciones</Caption>
+                            </View>
+                        </View>
+                    </View>
+
+                    <Drawer.Section style={styles.drawerSection}>
+                        <DrawerItem 
+                            icon={({color, size}) => (
+                                <Icon 
+                                name="map" 
+                                color={color}
+                                size={size}
+                                />
+                            )}
+                            label="Búsquedas"
+                            onPress={() => {
+                                navigation.navigate('Quest Navigator')
+                            }}
+                        />
+                        <DrawerItem 
+                            icon={({color, size}) => (
+                                <Icon 
+                                name="account-group" 
+                                color={color}
+                                size={size}
+                                />
+                            )}
+                            label="Amigos"
+                            onPress={() => {console.log('Amigos')}}
+                        />
+                        <DrawerItem 
+                            icon={({color, size}) => (
+                                <Icon 
+                                name="account-cog" 
+                                color={color}
+                                size={size}
+                                />
+                            )}
+                            label="Perfil"
+                            onPress={() => {console.log('Perfil')}}
+                        />
+                        <DrawerItem 
+                            icon={({color, size}) => (
+                                <Icon 
+                                name="ticket-percent" 
+                                color={color}
+                                size={size}
+                                />
+                            )}
+                            label="Cupones"
+                            onPress={() => {console.log('Cupones')}}
+                        />
+                        <DrawerItem 
+                            icon={({color, size}) => (
+                                <Icon 
+                                name="bell-badge" 
+                                color={color}
+                                size={size}
+                                />
+                            )}
+                            label="Notificaciones"
+                            onPress={() => {console.log('Notificaciones')}}
+                        />
+                    </Drawer.Section>
+                </View>
+            </DrawerContentScrollView>
+            <Drawer.Section style={styles.bottomDrawerSection}>
+                <DrawerItem 
+                    icon={({color, size}) => (
+                        <Icon 
+                        name="exit-to-app" 
+                        color={color}
+                        size={size}
+                        />
+                    )}
+                    label="Sign Out"
+                    onPress={() => {
+                        CloseSession()
+                        navigation.navigate('Sign In')
+                    }}
+                />
+            </Drawer.Section>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    drawerContent: {
+      flex: 1,
+    },
+    userInfoSection: {
+      paddingLeft: 20,
+    },
+    title: {
+      fontSize: 16,
+      marginTop: 3,
+      fontWeight: 'bold',
+    },
+    caption: {
+      fontSize: 14,
+      lineHeight: 14,
+    },
+    row: {
+      marginTop: 20,
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    section: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginRight: 15,
+    },
+    paragraph: {
+      fontWeight: 'bold',
+      marginRight: 3,
+    },
+    drawerSection: {
+      marginTop: 15,
+    },
+    bottomDrawerSection: {
+        marginBottom: 15,
+        borderTopColor: '#f4f4f4',
+        borderTopWidth: 1
+    },
+    preference: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      paddingVertical: 12,
+      paddingHorizontal: 16,
+    },
+  });
