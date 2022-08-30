da0017f3a9b178bc7003c44487bf901f7dee2a9a
PabloOve <31904880+PabloOve@users.noreply.github.com>
2022-08-30 00:26:52 -0300

    client quests WIP

--- app/components/screens/ClientQuests.jsx
+++ app/components/screens/ClientQuests.jsx
@@ -12,8 +12,25 @@ export default ClientQuests = ({route, navigation}) => {
     const {ID, name, image} = route.params
 
     const [data, setData] = useState([])
+    const [loading, setLoading] = useState(true)
 
     const url = Config.appUrl + "clients/" + ID + "/quests"
+
+    const getData = () => {
+        if (loading) {
+            return <ActivityIndicator size="large" />
+         }
+         return (
+            data.map( (quest, index) => 
+            <Pressable key={index}>
+                <View  style={styles.optionCard} key = {index}>
+                <Text style={{textAlign: 'center', marginTop:10, fontSize:18, fontWeight: 'bold'}}>{quest.name}</Text>
+                </View>        
+            </Pressable>
+            )
+         )
+    
+    }
 
     useEffect(() => {
         fetch(url)
@@ -23,9 +40,39 @@ export default ClientQuests = ({route, navigation}) => {
         .finally(()=>setLoading(false))
     }, [])
 
+    const Card = ({quest}) => {
+        return (
+        <View style={styles.card}>
+            <View
+                style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    marginTop: 10,
+                }}>
+                <Text style={{marginTop: 15, fontSize: 20, fontWeight: 'bold'}}>{quest.name}</Text>
+            </View>
+            <View style={{marginTop: 10, flexDirection: 'row'}}>
+                <View style={styles.questInfo}>
+                    <FontAwesome name ='clock-o' size={18}/>
+                    <Text style={styles.questInfoText}>{quest.duration}</Text>
+                </View>
+                <View style={styles.questInfo} marginTop={-50} marginLeft={20}>
+                    <Entypo name ='gauge' size={18}/>
+                    <Text style={styles.questInfoText}>{quest.difficulty}</Text>
+                </View>
+        </View>
+        </View>)}
+
     return (
         <ScrollView style={styles.view}>
-        <Text style = {styles.title}>{name}</Text> 
+        <Text style = {styles.title}>{name}</Text>
+        <FlatList
+            horizontal= {false}
+            contentContainerStyle={{paddingLeft: 20, paddingVertical: 20}}
+            showsHorizontalScrollIndicator = {false}
+            data={data}
+            renderItem={({item}) => <Card quest={item}/>
+        }></FlatList> 
         </ScrollView> 
     )
 }
@@ -35,9 +82,30 @@ const styles = StyleSheet.create({
         fontSize: 20,
         fontWeight: 'bold',
         color: '#a52a2a',
+        textAlign: 'center'
     },
     view: {
         flex: 1,
         backgroundColor: '#FFF9CA',
+    },
+    card:{
+        height: 100,
+        backgroundColor: '#ffefd5',
+        elevation: 10,
+        width: width - 65,
+        marginTop:20,
+        padding: 15,
+        borderRadius: 20,
+    },
+    questInfo: {
+        flexDirection: 'row',
+        marginTop: -50, 
+        marginLeft: 200,
+    },
+    questInfoText: {
+        marginTop: 20,
+        marginLeft: -25,
+        color: '#696969',
     }
 });
+
