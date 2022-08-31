16c8a11e183504947c87b0d9274e5ce76179dda4
PabloOve <31904880+PabloOve@users.noreply.github.com>
2022-08-31 01:10:30 -0300

    removing unused function

--- app/components/screens/ClientQuests.jsx
+++ app/components/screens/ClientQuests.jsx
@@ -17,64 +17,52 @@ export default ClientQuests = ({route, navigation}) => {
 
   const url = Config.appUrl + "clients/" + ID + "/quests"
 
-  const getData = () => {
-    if (loading) {
-      return <ActivityIndicator size="large" />
-    }
-  return (
-    data.map( (quest, index) => 
-      <Pressable key={index}>
-        <View  style={styles.optionCard} key = {index}>
-          <Text style={{textAlign: 'center', marginTop:10, fontSize:18, fontWeight: 'bold'}}>{quest.name}</Text>
-        </View>        
-      </Pressable>))
-    }
-
-    useEffect(() => {
-      fetch(url)
-        .then((response) => response.json())
-        .then((json) => setData(json))
-        .catch((error) => console.error(error))
-        .finally(()=>setLoading(false))
-    }, [])
+  useEffect(() => {
+    fetch(url)
+    .then((response) => response.json())
+    .then((json) => setData(json))
+    .catch((error) => console.error(error))
+    .finally(()=>setLoading(false))
+  }, [])
     
-    const Tags = ({tag}) => {
-      return (
-        <View style={styles.tag}>
-        <View style={{marginTop: -38, marginLeft:10}}>
-          <Text style={styles.tagInfoText}>{tag}</Text>
-        </View>
+  const Tags = ({tag}) => {
+    return (
+      <View style={styles.tag}>
+      <View style={{marginTop: -38, marginLeft:10}}>
+        <Text style={styles.tagInfoText}>{tag}</Text>
       </View>
-      )
-    }
-    const Card = ({quest}) => {
-      return (
-        <View style={styles.card}>
-          <View
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              marginTop: 10,
-            }}>
-            <Text style={{marginTop: 5, fontSize: 20, fontWeight: 'bold'}}>{quest.name}</Text>
+      </View>
+    )
+  }
+  
+  const Card = ({quest}) => {
+    return (
+      <View style={styles.card}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            marginTop: 10,
+          }}>
+          <Text style={{marginTop: 5, fontSize: 20, fontWeight: 'bold'}}>{quest.name}</Text>
+        </View>
+        <View style={{marginTop: 10, flexDirection: 'row'}}>
+          <View style={styles.questInfo}>
+            <FontAwesome name ='clock-o' size={25}/>
+            <Text style={styles.questInfoText}>{quest.duration}</Text>
           </View>
-          <View style={{marginTop: 10, flexDirection: 'row'}}>
-            <View style={styles.questInfo}>
-              <FontAwesome name ='clock-o' size={25}/>
-              <Text style={styles.questInfoText}>{quest.duration}</Text>
-            </View>
-            <View style={styles.questInfo} marginLeft={18}>
-              <Entypo name ='gauge' size={25}/>
-                <Text style={styles.questInfoText}>{quest.difficulty}</Text>
-            </View>
+        <View style={styles.questInfo} marginLeft={18}>
+          <Entypo name ='gauge' size={25}/>
+            <Text style={styles.questInfoText}>{quest.difficulty}</Text>
+        </View>
 
-            <View style={styles.questInfo} marginTop={-50} marginLeft={18}>
-              <Entypo name ='star' size={30}/>
-                <Text style={styles.questInfoText}>4.5</Text>
-            </View>
-            {quest.tags.map((tag) => <Tags tag={tag}/>)}  
-          </View>
-        </View>)}
+        <View style={styles.questInfo} marginTop={-50} marginLeft={18}>
+          <Entypo name ='star' size={30}/>
+          <Text style={styles.questInfoText}>4.5</Text>
+        </View>
+        {quest.tags.map((tag) => <Tags tag={tag}/>)}  
+        </View>
+      </View>)}
   
   return (
     <ScrollView style={styles.view}>
