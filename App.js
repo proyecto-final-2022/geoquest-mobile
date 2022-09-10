dc48254c1d4d3e6f70696191f261b76e255c8dbd
PabloOve <31904880+PabloOve@users.noreply.github.com>
2022-09-10 16:15:10 -0300

    notifications

--- App.js
+++ App.js
@@ -1,12 +1,52 @@
-import React, { useState } from "react";
-import { SafeAreaView, StyleSheet, ImageBackground, Text} from "react-native";
+import React, { useEffect, useState } from "react";
+import { SafeAreaView, StyleSheet, Alert, ImageBackground, Text} from "react-native";
 import { ViroARSceneNavigator } from "@viro-community/react-viro/components/AR/ViroARSceneNavigator";
 import { ViroARScene } from "@viro-community/react-viro/components/AR/ViroARScene";
 import { ViroText } from "@viro-community/react-viro/components/ViroText";
 import { ViroTrackingStateConstants } from "@viro-community/react-viro/components/ViroConstants";
 import Navigation from './app/components/navigation'
+import messaging from '@react-native-firebase/messaging'
 
 export default function App() {
+
+  useEffect(() => {
+    const foregroundSubscriber = messaging().onMessage(
+      async (remoteMessage) => {
+      Alert.alert(remoteMessage.notification.title)
+      console.log("push notification recibida", remoteMessage)
+    })
+
+    const topicSubscriber = messaging()
+    .subscribeToTopic('geoquest')
+    .then(() => console.log('suscrito a geoquest'))
+
+    const backgroundSubscriber = messaging()
+    .setBackgroundMessageHandler(async (remoteMessage) => {
+      console.log('Push notification en background', remoteMessage)
+    })
+
+
+    const getToken = messaging()
+    .getToken()
+    .then((token) => {
+      console.log("token:", token)
+    })
+
+    const tokenRefresh = messaging()
+    .getToken()
+    .then((token) => {
+      console.log("token refresh:", token)
+    })
+
+    return () => {
+      foregroundSubscriber();
+      topicSubscriber();
+      backgroundSubscriber();
+      getToken();
+      tokenRefresh();
+    }
+  }, [])
+
   return (
     <SafeAreaView style={styles.root}>
       <Navigation />
