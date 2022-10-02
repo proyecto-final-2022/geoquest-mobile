d2ae5fe3ebc361ceaa99692d4b2407af3684638a
PabloOve <31904880+PabloOve@users.noreply.github.com>
2022-10-02 17:02:56 -0300

    ranking flex wip

--- app/components/screens/Ranking.jsx
+++ app/components/screens/Ranking.jsx
@@ -1,11 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, ScrollView, Modal, ActivityIndicator, Text, View, Dimensions, Image, Pressable, FlatList, TouchableOpacity, TextInput} from 'react-native';
 import { useIsFocused } from '@react-navigation/native'
+import {Avatar} from 'react-native-paper';
 import {useNavigation} from '@react-navigation/native'
 import {FontAwesome, Entypo, Ionicons} from '@expo/vector-icons'
 import Config from '../../../config.json'
 import Tags from "react-native-tags"
 import CustomButton from '../commons/CustomButton'
+
+import userImage_1 from '../../../assets/userImages/userImage_1.png'
+import userImage_2 from '../../../assets/userImages/userImage_2.png'
+import userImage_3 from '../../../assets/userImages/userImage_3.png'
+import userImage_4 from '../../../assets/userImages/userImage_4.png'
+import userImage_5 from '../../../assets/userImages/userImage_5.png'
+import userImage_6 from '../../../assets/userImages/userImage_6.png'
+import userImage_7 from '../../../assets/userImages/userImage_7.png'
+import userImage_8 from '../../../assets/userImages/userImage_8.png'
+import userImage_9 from '../../../assets/userImages/userImage_9.png'
 
 const {width} = Dimensions.get('screen')
 
@@ -17,6 +28,8 @@ export default Ranking = ({route, navigation}) => {
   const urlTeam = Config.appUrl + "teams/rankings/" + id 
 
   const [ranking, setRanking] = useState([])
+  const [rankingIndex, setRankingIndex] = useState(0)
+
   const [teamRanking, setTeamRanking] = useState([])
   const [loading, setLoading] = useState(true)
 
@@ -76,24 +89,49 @@ export default Ranking = ({route, navigation}) => {
       </View>
   }
 
-  const Ranking = ({data}) => {
+  const getUserImage = (imageNumber) => { 
+    const userImages = [userImage_1, userImage_2, userImage_3, userImage_4, userImage_5, userImage_6, userImage_7, userImage_8, userImage_9];
+    return userImages[imageNumber-1];
+   }
+
+  const Ranking = ({data, index}) => {
     return(
       <View style={styles.rankingContainer}>
-        <View style={styles.rankingItem}>
+        <View style={styles.rankingItemPos}>
+          <Text style={{fontSize: 20, fontWeight: 'bold'}}>{index}</Text>
+        </View>
+        <View style={styles.rankingItemImage}>
+          <Avatar.Image 
+            source={getUserImage(data.image)}
+            size={50}
+            marginTop={5}
+          />
+        </View>
+        <View style={styles.rankingItemUsername}>
           <Text style={{fontSize: 20, fontWeight: 'bold'}}>{data.username}</Text>
         </View>
-        <View style={styles.rankingItem}>
+        <View style={styles.rankingItemTime}>
           <Text style={{fontSize: 20, fontWeight: 'bold'}}>{data.hours + "h " + data.minutes + "m " + data.seconds + "s "}</Text>
         </View>
       </View>
       )
   }
 
-  const TeamRanking = ({data}) => {
+  const TeamRanking = ({data}, index) => {
+    var index = 0
     return(
       <View style={styles.rankingContainer}>
+        
+        {console.log(index)}
         <View style={styles.rankingItem}>
-          {data.users.map((user) => <Text style={{fontSize: 20, fontWeight: 'bold'}}>{user}</Text>)}
+          {data.users.map((user, index) => 
+          {
+          <View>
+            <Text style={{fontSize: 20, fontWeight: 'bold'}}>{user}</Text>
+            <Text style={{fontSize: 20, fontWeight: 'bold'}}>{index}</Text>
+          </View>
+          })
+          }
         </View>
         <View style={styles.rankingItem}>
           <Text style={{fontSize: 20, fontWeight: 'bold'}}>{data.hours + "h " + data.minutes + "m " + data.seconds + "s "}</Text>
@@ -112,8 +150,14 @@ export default Ranking = ({route, navigation}) => {
           contentContainerStyle={{paddingLeft: 20, paddingVertical: 20}}
           showsHorizontalScrollIndicator = {false}
           data={ranking}
-          renderItem={({item}) => <Ranking data={item}/>
-        }></FlatList> :           
+          renderItem={({item, index}) => <Ranking data={item} index= {index + 1}/> }
+  //        {
+//    console.log(index + 1)
+  //          {console.log(index)}
+            //<Ranking data={item}/>
+//        }
+        //}
+        ></FlatList> :           
         <FlatList
           contentContainerStyle={{paddingLeft: 20, paddingVertical: 20}}
           showsHorizontalScrollIndicator = {false}
@@ -148,12 +192,34 @@ const styles = StyleSheet.create({
     paddingBottom: 5,
   },
   rankingContainer: {
+    backgroundColor:'antiquewhite',
+    marginTop: 5,
+    height: 70,
     flexDirection: 'row',
+    alignItems: 'center'
+  },
+  rankingItemPos: {
+    flexBasis: 30,
+    flexShrink: 0,
+    flexGrow: 0,
+  },
+  rankingItemImage: {
+    flexBasis: 60,
+    flexShrink: 0,
+    flexGrow: 0,
+  },
+  rankingItemUsername: {
+    flexBasis: 100,
+    flexShrink: 0,
+    flexGrow: 0,
+  },
+  rankingItemTime: {
+    flexBasis: 200,
+    flexShrink: 0,
+    flexGrow: 0,
   },
   rankingItem: {
-    width: 200,
-    marginTop: 20,
-    backgroundColor: '#ffefd5',
+
   }
 });
 
