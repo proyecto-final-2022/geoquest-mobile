1fb9755b03a3317fd7990e1939ad2cd12185b8ef
isarasibar <sarasibar.ignacio@gmail.com>
2022-11-22 03:19:55 -0300

    Fix acentos

--- app/components/screens/ProfileScreen.jsx
+++ app/components/screens/ProfileScreen.jsx
@@ -193,19 +193,19 @@ const ProfileScreen = ({navigation}) => {
         setAchivements([
             {
                 name: "Haz un amigo",
-                description: "Envia una invitación de amistad exitosa o acepta una",
+                description: "Enviá una invitación de amistad exitosa o aceptá una",
                 logo: friend_icon,
                 earned: userAchivements.includes("MadeFriend_ac")
             },
             {
                 name: "Inicia una búsqueda",
-                description: "Arranca cualquier búsqueda",
+                description: "Arrancá cualquier búsqueda",
                 logo: start_icon,
                 earned: userAchivements.includes("StartedQuest_ac")
             },
             {
                 name: "Completa una búsqueda",
-                description: "Termina una búsqueda y obtén tu recompenza",
+                description: "Terminá una búsqueda y obtén tu recompenza",
                 logo: finish_icon,
                 earned: userAchivements.includes("FinishedQuest_ac")
             },
@@ -223,7 +223,7 @@ const ProfileScreen = ({navigation}) => {
             },
             {
                 name: "Usa un cupón",
-                description: "Utiliza uno de los cupones obtenidos como recompenza",
+                description: "Utilizá uno de los cupones obtenidos como recompensa",
                 logo: coupon_icon,
                 earned: userAchivements.includes("UsedCoupon_ac")
             }
